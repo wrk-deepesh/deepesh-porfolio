4e0022c5b04aa5d7239a213b46d513d74accb388
dev-deepesh <121635857+dev-deepesh@users.noreply.github.com>
2024-05-26 23:11:59 +0530

    Add Base Files for animation & navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+  const [ navLink, setNavLink ] = useState('home');
+  return (
+    <div style={{
+        width: "100%",
+        height: "50px"
+    }}>
+        
+    </div>
+  )
+}
+
+export default Navbar
